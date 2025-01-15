655d8cf462be975af35f4ae139affaebddd31798
root <root@root.com>
2025-01-15 21:23:59 +0300

    CBV in main application

--- main/urls.py
+++ main/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
-from . import views
+from .views import *
 
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.popular_list, name='popular_list'),
-    path('shop/', views.product_list, name='product_list'),
-    path('shop/<slug:slug>/', views.product_detail,
+    path('',
+         PopularListView.as_view(),
+         name='popular_list'),
+    path('shop/',
+         ProductListView.as_view(),
+         name='product_list'),
+    path('shop/<slug:slug>/',
+         ProductDetailView.as_view(),
          name='product_detail'),
-    path('shop/category/<slug:category_slug>', views.product_list,
-         name='product_list_by_category'),
+    path('shop/category/<slug:category_slug>',
+          ProductListView.as_view(),
+          name='product_list_by_category'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,44 +2,92 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from cart.forms import CartAddProductForm
+from django.views.generic import ListView, DetailView
 
 
-def popular_list(request):
-    products = Product.objects.filter(available=True)[:3]
-    return render(request, 'main/index/index.html',
-                  {'products': products})
+class PopularListView(ListView):
+    model = Product
+    template_name = 'main/index/index.html'
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        return Product.objects.filter(available=True)[:3]
 
 
-def product_detail(request, slug):
-    product = get_object_or_404(Product,
-                                slug=slug,
-                                available=True)
+# def popular_list(request):
+#     products = Product.objects.filter(available=True)[:3]
+#     return render(request, 'main/index/index.html',
+#                   {'products': products})
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'main/product/detail.html'
     form = CartAddProductForm
-    return render(request, 'main/product/detail.html',
-                  {'product': product,
-                   'form': form})
+    context_object_name = 'product'
+
+    def get_object(self, queryset = None):
+        return get_object_or_404(Product, slug=self.kwargs[self.slug_url_kwarg], available=True)
+    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = self.form
+        return context
+
+# def product_detail(request, slug):
+#     product = get_object_or_404(Product,
+#                                 slug=slug,
+#                                 available=True)
+#     form = CartAddProductForm
+#     return render(request, 'main/product/detail.html',
+#                   {'product': product,
+#                    'form': form})
 
 
-def product_list(request, category_slug=None):
-    page = request.GET.get('page', 1)
-    category = None
-    categories = Category.objects.all()
-    products = Product.objects.filter(available=True)
+class ProductListView(ListView):
+    model = Product
+    template_name = 'main/product/list.html'
+    context_object_name = 'products'
+    paginate_by = 3
+
+    def get_queryset(self):
+        products = Product.objects.filter(available=True)
+        category_slug = self.kwargs.get('category_slug')
+        if category_slug:
+            category = get_object_or_404(Category, slug=category_slug)
+            products = products.filter(category=category)
+        return products
     
-    # Фильтрация по категории
-    if category_slug:
-        category = get_object_or_404(Category, slug=category_slug)
-        products = products.filter(category=category)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_slug = self.kwargs.get('category_slug')
+        if category_slug:
+            category = get_object_or_404(Category, slug=category_slug)
+            context['category'] = category
+        context['categories'] = Category.objects.all()
+        return context
+
+# def product_list(request, category_slug=None):
+#     page = request.GET.get('page', 1)
+#     category = None
+#     categories = Category.objects.all()
+#     products = Product.objects.filter(available=True)
     
-    # Пагинация
-    paginator = Paginator(products, 10)
-    try:
-        current_page = paginator.page(int(page))
-    except EmptyPage:
-        current_page = paginator.page(paginator.num_pages)
+#     # Фильтрация по категории
+#     if category_slug:
+#         category = get_object_or_404(Category, slug=category_slug)
+#         products = products.filter(category=category)
     
-    return render(request, 'main/product/list.html', {
-        'category': category,
-        'categories': categories,
-        'products': current_page,
-    })
+#     # Пагинация
+#     paginator = Paginator(products, 10)
+#     try:
+#         current_page = paginator.page(int(page))
+#     except EmptyPage:
+#         current_page = paginator.page(paginator.num_pages)
+    
+#     return render(request, 'main/product/list.html', {
+#         'category': category,
+#         'categories': categories,
+#         'products': current_page,
+#     })
