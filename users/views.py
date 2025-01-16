ff1606ab80393a18da895a78c5fb93eb67f54be5
root <root@root.com>
2025-01-16 20:05:35 +0300

    CBV in users views

--- users/views.py
+++ users/views.py
@@ -1,70 +1,60 @@
-from django.shortcuts import render, redirect
-from django.contrib import auth, messages
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.shortcuts import redirect
 from .forms import UserLoginForm, UserRegistrationFrom, ProfileForm
-from django.contrib.auth.decorators import login_required
-from django.db.models import Prefetch
 from orders.models import Order, OrderItem
+from django.db.models import Prefetch
 
 
-def login(request):
-    if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = auth.authenticate(username=username,
-                                      password=password)
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'
+    form_class = UserLoginForm
+    redirect_authenticated_user = True
 
-            if user:
-                auth.login(request, user)
-                return HttpResponseRedirect(reverse_lazy('main:product_list'))
-    else:
-        form = UserLoginForm()
-
-    return render(request, 'users/login.html', {'form': form})
+    def get_success_url(self):
+        return reverse_lazy('main:product_list')
 
 
-def registration(request):
-    if request.method == 'POST':
-        form = UserRegistrationFrom(data=request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.instance
-            auth.login(request, user)
-            messages.success = (
-                request, f'{user.username}, Successful Registration'
+class UserRegistrationView(CreateView):
+    template_name = 'users/registration.html'
+    form_class = UserRegistrationFrom
+    success_url = reverse_lazy('user:login')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = form.instance
+        auth.login(self.request, user)
+        messages.success(self.request, f'{user.username}, Successful Registration')
+        return redirect(self.get_success_url())
+    
+
+class UserProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'users/profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = ProfileForm(instance=self.request.user)
+
+        # Заказы пользователя
+        context['orders'] = Order.objects.filter(user=self.request.user).prefetch_related(
+            Prefetch(
+                'items',
+                queryset=OrderItem.objects.select_related('product')
             )
-            return HttpResponseRedirect(reverse_lazy('user:login'))
-    else:
-        form = UserRegistrationFrom
+        ).order_by('-id')
+        return context
 
-    return render(request, 'users/registration.html')
-
-
-@login_required
-def profile(request):
-    if request.method == 'POST':
+    def post(self, request, *args, **kwargs):
         form = ProfileForm(request.POST, instance=request.user, files=request.FILES)
         if form.is_valid():
             form.save()
-            messages.success = (request, 'Profile was changed')
-            return HttpResponseRedirect(reverse_lazy('user:profile'))
-    else:
-        form = ProfileForm(instance=request.user)
-    
-    orders = Order.objects.filter(user=request.user).prefetch_related(
-        Prefetch(
-            'items',
-            queryset=OrderItem.objects.select_related('product')
-        )
-    ).order_by('-id')
-
-    return render(request, 'users/profile.html', {'form': form,
-                                                  'orders': orders})
-
-
-def logout(request):
-    auth.logout(request)
-    return redirect(reverse_lazy('main:product_list'))
+            messages.success(request, 'Profile was changed')
+            return redirect(reverse_lazy('user:profile'))
+        else:
+            # Если форма невалидна, передаем её в контекст
+            context = self.get_context_data()
+            context['form'] = form
+            return self.render_to_response(context)
